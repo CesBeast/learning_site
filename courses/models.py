f2c28a7bab086c0cf0ba17ab04ad3f237edf6b31
Cesar Garcia <cesbeast@gmail.com>
2017-04-18 16:27:56 -0700

    Added steps, which lists the steps in the courses

--- courses/models.py
+++ courses/models.py
@@ -2,12 +2,23 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# model for a course.
 class Course(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True)
 	title = models.CharField(max_length=255)
 	description = models.TextField()
 
 	# If a type string is called on this object, it will return the title
+	def __str__(self):
+		return self.title
+
+
+# model for a step in a course
+class Step(models.Model):
+	title = models.CharField(max_length=255)
+	description = models.TextField()
+	order = models.IntegerField(default = 0)
+	course = models.ForeignKey(Course)
+
 	def __str__(self):
 		return self.title
