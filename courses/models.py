adc2db423e9967b6f31c1345c54a3a4eb258dd82
cesar garcia <cesar@cesars-MacBook-Pro.local>
2017-04-17 23:21:08 -0700

    added def type string to course model and set up admins.

--- courses/models.py
+++ courses/models.py
@@ -7,3 +7,7 @@ class Course(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True)
 	title = models.CharField(max_length=255)
 	description = models.TextField()
+
+	# If a type string is called on this object, it will return the title
+	def __str__(self):
+		return self.title
