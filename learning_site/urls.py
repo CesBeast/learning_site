9a6bda1fc4088669dae50a77bf1cb87127ed1f0b
Cesar Garcia <cesbeast@gmail.com>
2017-04-18 01:56:19 -0700

    Added a home.html file with the URL

--- learning_site/urls.py
+++ learning_site/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
+	url(r'^$', views.home),
     url(r'^admin/', admin.site.urls),
     url(r'^courses/', include('courses.urls')),
     
