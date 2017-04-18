23e8fcbe5c6875dc087f6d95104e8e00b214d951
cesar garcia <cesar@cesars-MacBook-Pro.local>
2017-04-17 22:49:59 -0700

    Added courses url, which will return the title of the courses

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Course
+
 # Create your views here.
+
+def course_list(request):
+	courses = Course.objects.all()
+	output = ""
+	for course in courses:
+		output = output+" "+str(course.title)
+	return HttpResponse(output)
--- learning_site/views.py
+++ learning_site/views.py
@@ -1,4 +1,2 @@
 from django.http import HttpResponse
-
-def hello_world(request):
-    return HttpResponse('Hello World')
+from django.shortcuts import render
