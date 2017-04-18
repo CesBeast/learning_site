36fce06125ec365856f4deb77fff1d8285106a38
Cesar Garcia <cesbeast@gmail.com>
2017-04-18 16:34:24 -0700

    added steps as inlines to admins

--- courses/admin.py
+++ courses/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 from .models import Course, Step
 
 # Register your models here.
-admin.site.register(Course)
+
+class StepInLine(admin.StackedInline):
+	model = Step
+
+class CourseAdmin(admin.ModelAdmin):
+	inlines = [StepInLine,]
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Step)
